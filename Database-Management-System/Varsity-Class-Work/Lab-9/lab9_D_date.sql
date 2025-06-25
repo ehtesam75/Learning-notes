9a66451b5879e6adb1d18ef60bcf4d73a4f5edee
ehtesam75 <ehtesamzunnuryn@gmail.com>
2025-06-25 20:04:57 +0600

    update lab9 of sql

--- Database-Management-System/Varsity-Class-Work/Lab-9/lab9_D_date.sql
+++ Database-Management-System/Varsity-Class-Work/Lab-9/lab9_D_date.sql
@@ -22,14 +22,44 @@ select sysdate from dual;
 -- 25-JUN-25
 
 
---add or substract monnths
+--add or substract months
 select add_months(sysdate, 3) as plus_3_months from dual;  --adds 3 months to current date
+-- PLUS_3_MONTHS
+-- -------------
+-- 25-SEP-25
 
 select add_months(sysdate, -3) as minus_3_months from dual; --goes back 3 months
+-- MINUS_3_MONTHS
+-- --------------
+-- 25-MAR-25
+
 
 --find later or earlier date
 select greatest(sysdate, to_date('01-jan-2022', 'dd-mm-yyyy')) from dual;
 -- GREATEST(
 -- ---------
 -- 25-JUN-25
+
+
+--least date
+select least(sysdate, to_date('01-jan-2022', 'dd-mm-yyyy')) from dual;
+-- LEAST(   
+-- ---------
+-- 01-JAN-22
+
+
+-- get year/month from date
+select extract(year from sysdate) as current_year from dual;
+-- CURRENT_YEAR
+-- ------------
+--         2025
+
+
+--last day of the month
+select last_day(sysdate) from dual;
+-- LAST_DAY(
+-- ---------
+-- 30-JUN-25
+
+
 
