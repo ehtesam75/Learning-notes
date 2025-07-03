9c20ffb4993187c71d5965cf3ef3c232cc0feb9e
ehtesam75 <ehtesamzunnuryn@gmail.com>
2025-07-03 21:23:22 +0600

    add print method and comments to g_switch_function

--- Java/Code/g_switch_function.java
+++ Java/Code/g_switch_function.java
@@ -1,4 +1,10 @@
 public class g_switch_function {
+    public static void print(){
+        //why use static here?
+        //because we are calling this method from a static context (main method)
+        System.out.println("hello world");
+    }
+
     public static void main(String[] args) {
         String day = "tuesday";
         switch (day){
@@ -14,5 +20,12 @@ public class g_switch_function {
             default:
                 System.out.println("some other day:)");
         }
+
+        //CALLING A METHOD
+        print();
+        // note:
+        // Java only has METHODS - it doesn't have standalone functions like some other languages.
+        // Method in Java:
+        // A block of code that belongs to a class
     }
 }
